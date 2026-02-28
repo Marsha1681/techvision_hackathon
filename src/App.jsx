55023556bcd34a949cfe2de076fd5837e0fa7455
Zayna Sheikh <zsheikh2@sheffield.ac.uk>
2026-02-28 13:40:22 +0000

    Shopping cart movement basics

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import { motion } from "framer-motion";
 import './App.css'
 
 function App() {
@@ -10,12 +11,21 @@ function App() {
         <div id="item-list">
             <h2>Browse Items</h2>
         </div>
-
-      <div>
-        Cart here
+        <div>
+          <motion.img
+            src="/shopping-cart.png"
+            alt="Shopping cart image"
+            initial={{ x: 0, y: 0 }}
+            animate={{ x: [0, 200, 0] }}
+            transition={{
+              duration: 2,
+              repeat: Infinity,
+              repeatType: "loop",
+              ease: "linear"
+            }}
+          />
+        </div>
       </div>
-    </div>
-
     </>
   )
 }
