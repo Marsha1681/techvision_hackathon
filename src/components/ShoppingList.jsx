ba6ce483a0e6a6ebf04dbf52ae6e140c4313df87
tanktunk <tanvikumar12907@gmail.com>
2026-02-28 17:46:02 +0000

    changed images and also added prices

--- src/components/ShoppingList.jsx
+++ src/components/ShoppingList.jsx
@@ -3,16 +3,16 @@ import "./ShoppingList.css";
 import SlingshotItem from "./SlingshotItem";
 
 const items = [
-  { id: 1, name: "Dress", image: "/dress.png" },
-  { id: 2, name: "Earring", image: "/earring.png" },
-  { id: 3, name: "Purse", image: "/purse.png" },
-  { id: 4, name: "Shoes", image: "/shoes.png" },
-  { id: 5, name: "Necklace", image: "/necklace.png" },
-  { id: 6, name: "Skirt", image: "/skirt.png" },
-  { id: 7, name: "Coat", image: "/coat.png" },
-  { id: 8, name: "Makeup", image: "/makeup.png" },
-  { id: 9, name: "Scarf", image: "/scarf.png" },
-  { id: 10, name: "Perfume", image: "/perfume.png" },
+  { id: 1, name: "Dress", price: 29.99, image: "/dress.png" },
+  { id: 2, name: "Earring", price: 14.99, image: "/earring.png" },
+  { id: 3, name: "Purse", price: 39.99, image: "/purse.png" },
+  { id: 4, name: "Shoes", price: 59.99, image: "/shoes.png" },
+  { id: 5, name: "Necklace", price: 24.99, image: "/necklace.png" },
+  { id: 6, name: "Skirt", price: 19.99, image: "/skirt.png" },
+  { id: 7, name: "Coat", price: 49.99, image: "/coat.png" },
+  { id: 8, name: "Makeup", price: 12.99, image: "/makeup.png" },
+  { id: 9, name: "Scarf", price: 8.99, image: "/scarf.png" },
+  { id: 10, name: "Perfume", price: 34.99, image: "/perfume.png" },
 ];
 
 export default function ShoppingList({ onAddToCart }) {
